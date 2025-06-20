325890fd99033eb820d47b0bb23f97d4e80254b6
ojhaugen15 <ojhaugen15@gmail.com>
2025-06-20 11:20:56 -0700

    Update runProgram.js

--- runProgram.js
+++ runProgram.js
@@ -1,6 +1,1 @@
-function runProgram (programText) {
-  programElement = document.createElement('script')
-  programElement.text = programText
-  bodyNode = document.body
-  bodyNode.appendChild(programElement)
-}
+
